043bd8c2de0d59582c7c7a09e0fff4bf410437b3
pranav madhu <pranavmadhu73@gmail.com>
2022-10-24 17:24:37 +0530

    added html and data for footer section

---
+++ components/Footer.js
@@ -0,0 +1,27 @@
+import styles from "../styles/Footer.module.css";
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <span>Innovators Premier League</span>
+      </div>
+      <div>
+        <p>
+          Address:Thejaswini, G3B, Technopark Rd, Karyavattom,
+          Thiruvananthapuram, Kerala 695581
+        </p>
+        <span>Phone: 0471-2700270</span>
+        <span>Email: iedckerala@startupmission.in</span>
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        <small>
+          Copyright © 2021 Kerala Startup Mission. All Rights Reserved.
+        </small>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import About from "../components/About";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <Hero />
       <About />
+      <Footer />
     </div>
   );
 }
