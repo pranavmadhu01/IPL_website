78cd6d070b91d91b9739b0a830c979ca12790f0e
pranav madhu <pranavmadhu73@gmail.com>
2022-10-30 21:47:43 +0530

    added dropdown in nabar

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
 import styles from "../styles/Header.module.css";
 // import logo from "../public/assets/ksum.png";
 import logo from "../public/assets/ipl_white.png";
-import  {BiMenuAltRight}  from "react-icons/bi";
-import {AiOutlineCloseSquare} from "react-icons/ai";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineCloseSquare } from "react-icons/ai";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { IoMdArrowDropdown } from "react-icons/io";
 const Header = () => {
   const anchors = [
     "home",
@@ -22,8 +24,9 @@ const Header = () => {
   ];
   const [scrollY, setScrollY] = useState(true);
   const [showMenu, setShowMenu] = useState(false);
+  const [dropdownActive, setDropdownAcitve] = useState(false);
   const router = useRouter();
-  const nav =useRef(null);
+  const nav = useRef(null);
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 300) {
@@ -33,57 +36,68 @@ const Header = () => {
       }
     };
     // if(window.innerWidth>968){
-      handleScroll();
+    handleScroll();
     // }
-    
+
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-  const handleMenuClick = (path)=>{
-    handleCloseOPen()
-    router.push(`#${[path]}`)
-  }
-  
+  const handleMenuClick = (path, index) => {
+    handleCloseOPen(index);
+    router.push(`#${[path]}`);
+  };
 
-  const handleCloseOPen = () => {
-    if(nav!==null){
+  const handleCloseOPen = (index) => {
+    if (nav !== null && index !== 5) {
       nav.current.classList.toggle(styles.open);
       setShowMenu(!showMenu);
     }
-  }
-
+  };
 
   return (
     <header className={scrollY ? styles.header : styles.header_active}>
       <div className={styles.nav_logo}>
-      <Image
-        src={logo}
-        alt="kerala startup mission logo"
-      />
+        <Image src={logo} alt="kerala startup mission logo" />
       </div>
-      <nav >
+      <nav>
         <ul className={styles.header_nav_list} ref={nav}>
-          {anchors.map((anchor) => (
+          {anchors.map((anchor, index) => (
             <li>
-              <p onClick={()=>{handleMenuClick(anchor)}} className={styles.header_nav_list_anchor}>
+              <p
+                onClick={() => {
+                  handleMenuClick(anchor, index);
+                  index === 5 && setDropdownAcitve(!dropdownActive);
+                }}
+                className={styles.header_nav_list_anchor}
+              >
                 {anchor}
+                {index === 5 && <IoMdArrowDropdown />}
+                {dropdownActive && index === 5 && (
+                  <div className={styles.header_dropdown}>
+                    <a
+                      href="https://ipl.startupmission.in/1.0/"
+                      target="_blank_"
+                      style={{ textDecoration: "none", color: "#ef8354" }}
+                    >
+                      IPL 1.0
+                    </a>
+                  </div>
+                )}
               </p>
             </li>
           ))}
-          
         </ul>
         <div className={styles.mobile_menu_btn}>
-          {showMenu?<AiOutlineCloseSquare onClick={handleCloseOPen} size={50} />:<BiMenuAltRight onClick={handleCloseOPen} size={50}/>}
-        
+          {showMenu ? (
+            <AiOutlineCloseSquare onClick={handleCloseOPen} size={50} />
+          ) : (
+            <BiMenuAltRight onClick={handleCloseOPen} size={50} />
+          )}
         </div>
-
-        
       </nav>
-
-
     </header>
   );
 };
