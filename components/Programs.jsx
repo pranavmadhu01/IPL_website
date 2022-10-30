15d6f4b076a5bd5c83b0fb67645be2152e5f0a03
pranav madhu <pranavmadhu73@gmail.com>
2022-10-31 01:13:56 +0530

    added some images in programs

--- components/Programs.jsx
+++ components/Programs.jsx
@@ -1,32 +1,29 @@
-import Image from 'next/image'
-import { isMobile } from 'react-device-detect';
-import styles from '../styles/Programs.module.css'
+import Image from "next/image";
+import { isMobile } from "react-device-detect";
+import styles from "../styles/Programs.module.css";
 
-import img from '../public/assets/img5.jpg'
+import img from "../public/assets/img5.jpg";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Pagination,Navigation, Autoplay } from "swiper";
-import { useEffect, useState } from 'react';
+import { Pagination, Navigation, Autoplay } from "swiper";
+import { useEffect, useState } from "react";
 
-
-export default function Programs(){
-
-  const [slidesPerView, setslidesPerView] = useState(5.5)
-  const [spaceBetween, setspaceBetween] = useState(20)
+export default function Programs() {
+  const [slidesPerView, setslidesPerView] = useState(5.5);
+  const [spaceBetween, setspaceBetween] = useState(20);
 
   useEffect(() => {
-    if(isMobile){
-      setslidesPerView(1.5)
-      setspaceBetween(10)
+    if (isMobile) {
+      setslidesPerView(1.5);
+      setspaceBetween(10);
     }
-  }, [])
-  
+  }, []);
 
   return (
-    <div className={styles.pgm_arr_top} id='programs'>   
+    <div className={styles.pgm_arr_top} id="programs">
       <Swiper
         slidesPerView={slidesPerView}
         spaceBetween={spaceBetween}
@@ -41,62 +38,68 @@ export default function Programs(){
         //   clickable: true,
         // }}
         navigation={true}
-        modules={[Autoplay,Pagination, Navigation]}
+        modules={[Autoplay, Pagination, Navigation]}
         className={styles.mySwiper}
       >
-        {event_data.map(event=>{
-          return(
+        {event_data.map((event) => {
+          return (
             <SwiperSlide>
-              <div className={styles.pgm_card_top}>  
+              <div className={styles.pgm_card_top}>
                 <div className={styles.pgm_img}>
-                  <Image src={img} width={250} height={350}/>
+                  <Image
+                    src={event.image}
+                    width={250}
+                    height={250}
+                    objectFit="cover"
+                  />
                 </div>
                 <div className={styles.pgm_cont}>
                   <p>{event.name}</p>
                 </div>
               </div>
             </SwiperSlide>
-          )
+          );
         })}
       </Swiper>
     </div>
-  )
+  );
 }
 
 const event_data = [
   {
-    name:'Hackathons',
-    image:'random.png'
+    name: "Hackathons",
+    image: "/../public/assets/img5.jpg",
   },
   {
-    name:'Ideathons',
-    image:'random.png'
-  },{
-    name:'Startup Pitch',
-    image:'random.png'
+    name: "Ideathons",
+    image: "/../public/assets/bg3.jpg",
   },
   {
-    name:'Product Showcase',
-    image:'random.png'
+    name: "Startup Pitch",
+    image: "/../public/assets/img5.jpg",
   },
   {
-    name:'iTalk',
-    image:'random.png'
+    name: "Product Showcase",
+    image: "/../public/assets/img7.jpeg",
   },
   {
-    name:'Bootcamps',
-    image:'random.png'
+    name: "iTalk",
+    image: "/../public/assets/italk.jpg",
   },
   {
-    name:'Workshops',
-    image:'random.png'
+    name: "Bootcamps",
+    image: "/random.png",
   },
   {
-    name:'FabLab Visits',
-    image:'random.png'
+    name: "Workshops",
+    image: "/../public/assets/bg1.jpg",
   },
   {
-    name:'Experience Sessions',
-    image:'random.png'
-  }
-]
+    name: "FabLab Visits",
+    image: "/random.png",
+  },
+  {
+    name: "Experience Sessions",
+    image: "/../public/assets/img6.jpg",
+  },
+];
